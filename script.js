f24e8f8f17f4de60a4c59691a2c6e9bea9f615e9
Otto Micheletti <michelettiotto@gmail.com>
2021-12-10 16:04:22 -0300

    Requisito 5 e 6

---
+++ script.js
@@ -0,0 +1,22 @@
+// Requisito 5
+
+function createNewTask() {
+  let newTask = document.querySelector('#texto-tarefa');
+  let newLiItem = document.createElement('li');
+  let olItem = document.querySelector('#lista-tarefas');
+
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('criar-tarefa')) {
+      if (newTask.value.length > 0) {
+        let newLiItem = document.createElement('li');
+        newLiItem.innerText = newTask.value;
+
+        olItem.appendChild(newLiItem);
+        newTask.value = '';
+      } else {
+        alert('Error: Digite ao menos 1 caractere.');
+      }
+    }
+  });
+}
+createNewTask();
